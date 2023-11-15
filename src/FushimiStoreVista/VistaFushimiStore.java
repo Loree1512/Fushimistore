ef6ac10d1b7ee32827f92b843f9609696ea2e06a
Kate <Kate@KTHLN>
2023-11-14 21:24:20 -0300

    Se modifico el fondo, se agrego boton de Salir, Logo, usuario y contraseña. Se terminaron los ajustes esteticos!

--- src/FushimiStoreVista/VistaFushimiStore.java
+++ src/FushimiStoreVista/VistaFushimiStore.java
@@ -29,9 +29,13 @@ public class VistaFushimiStore extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabelBotonSalir = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
         jLabelLogo = new javax.swing.JLabel();
         jLabelUsuario = new javax.swing.JLabel();
         jLabelContraseña = new javax.swing.JLabel();
+        jTextFieldUsuario = new javax.swing.JTextField();
+        jPasswordFieldContraseña = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
         jLabelFondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -52,18 +56,40 @@ public class VistaFushimiStore extends javax.swing.JFrame {
         });
         getContentPane().add(jLabelBotonSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 10, 70, -1));
 
+        jLabel1.setFont(new java.awt.Font("Tw Cen MT Condensed", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(92, 24, 125));
+        jLabel1.setText("Fushimi Store");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 300, 120, -1));
+
         jLabelLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/FushimiStoreImg/BunnyCatLogo-removebg-preview.png"))); // NOI18N
-        getContentPane().add(jLabelLogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, 430, 230));
+        getContentPane().add(jLabelLogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 60, 430, 230));
 
         jLabelUsuario.setFont(new java.awt.Font("Tw Cen MT", 1, 24)); // NOI18N
         jLabelUsuario.setForeground(new java.awt.Color(92, 24, 125));
         jLabelUsuario.setText("Usuario: ");
-        getContentPane().add(jLabelUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 400, -1, -1));
+        getContentPane().add(jLabelUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 400, -1, -1));
 
         jLabelContraseña.setFont(new java.awt.Font("Tw Cen MT", 1, 24)); // NOI18N
         jLabelContraseña.setForeground(new java.awt.Color(92, 24, 125));
         jLabelContraseña.setText("Contraseña:");
-        getContentPane().add(jLabelContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 460, -1, -1));
+        getContentPane().add(jLabelContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 460, -1, -1));
+
+        jTextFieldUsuario.setBackground(new java.awt.Color(247, 239, 255));
+        jTextFieldUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldUsuarioActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextFieldUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 400, 220, 30));
+
+        jPasswordFieldContraseña.setBackground(new java.awt.Color(247, 239, 255));
+        getContentPane().add(jPasswordFieldContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 460, 220, 30));
+
+        jButton1.setBackground(new java.awt.Color(248, 233, 248));
+        jButton1.setFont(new java.awt.Font("Tw Cen MT", 1, 18)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(92, 24, 125));
+        jButton1.setText("Ingresar");
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 550, 150, -1));
 
         jLabelFondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/FushimiStoreImg/fondo_blanco_corazon_morado.jpg"))); // NOI18N
         jLabelFondo.setBorder(javax.swing.BorderFactory.createEtchedBorder(new java.awt.Color(204, 0, 204), null));
@@ -77,6 +103,10 @@ public class VistaFushimiStore extends javax.swing.JFrame {
     private void jLabelBotonSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jLabelBotonSalirActionPerformed
       System.exit(0);
     }//GEN-LAST:event_jLabelBotonSalirActionPerformed
+
+    private void jTextFieldUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextFieldUsuarioActionPerformed
 
     
     /**
@@ -113,11 +143,15 @@ public class VistaFushimiStore extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JButton jLabelBotonSalir;
     private javax.swing.JLabel jLabelContraseña;
     private javax.swing.JLabel jLabelFondo;
     private javax.swing.JLabel jLabelLogo;
     private javax.swing.JLabel jLabelUsuario;
+    private javax.swing.JPasswordField jPasswordFieldContraseña;
+    private javax.swing.JTextField jTextFieldUsuario;
     // End of variables declaration//GEN-END:variables
 }
 
